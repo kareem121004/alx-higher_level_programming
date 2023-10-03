e2604021e558ac42c1225e89255a0303a2d6b8db
kareem121004 <karimadel11112004@gmail.com>
2023-10-03 12:58:51 -0700

    commit again for 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result_str = ""
     for char in str:
-        if 'a' <= char <= 'z':
-            uppercase_char = chr(ord(char) - 32)
-            result_str += uppercase_char
-        else:
-            result_str += char
-    print("{}".format(result))
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
