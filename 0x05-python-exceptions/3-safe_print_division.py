7780069f4d91d95a7e7b9d3548a19ea0423474d7
kareem121004 <karimadel11112004@gmail.com>
2023-10-23 04:40:04 -0700

    commit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,10 @@
 def safe_print_division(a, b):
 	result = 0
 
-	try:
-		result = a / b
-	except ZeroDivisionError:
-		result = None
-	finally:
-		print("Inside result: {}".format(result))
-		return result
+        try:
+            result = a / b
+        except ZeroDivisionError:
+            result = None
+        finally:
+            print("Inside result: {}".format(result))
+            return result
