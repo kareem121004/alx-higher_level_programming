0442619eb49d81c23e7b9c8460ad62c36bd67c02
kareem121004 <karimadel11112004@gmail.com>
2023-10-24 10:55:28 -0700

    commit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -33,8 +33,7 @@ class Square:
     def my_print(self):
         """Prints the square with the # character."""
         for i in range(0, self.__size):
-             for j in range(self.__size):
-                  print("#", end="")
-             print("")
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
             print("")
