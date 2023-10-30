e7060398fce642caf1610f8a5f68d4d2c01519c9
kareem121004 <karimadel11112004@gmail.com>
2023-10-30 11:05:57 -0700

    commit

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -78,6 +78,7 @@ class Rectangle:
             return rect_1
         if rect_1.area() < rect_2.area():
             return rect_2
+
     @classmethod
     def square(cls, size=0):
         return cls(size, size)
