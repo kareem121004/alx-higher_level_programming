73f79e35960db34e9ee9bdfb15b5e6c93362f84e
kareem121004 <karimadel11112004@gmail.com>
2023-11-06 03:38:09 -0800

    commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a Square"""
+
+    def __init__(self, size):
+        """Initializes an instance"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Computes the area of a Square instance"""
+        return self.__size ** 2
