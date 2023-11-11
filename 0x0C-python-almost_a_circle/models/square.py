5fc7403b72460c01f90769288f6bbfa8456f6159
kareem121004 <karimadel11112004@gmail.com>
2023-11-11 01:23:12 -0800

    commit

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,52 @@
+# !/usr/bin/python3
+"""Module for Square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square implementstion"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialization"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation"""
+        id = self.id
+        size = self.__size
+        x = self.x
+        y = self.y
+        return "[Square] ({}) {}/{} - {}".format(id, x, y, size)
+
+    @property
+    def size(self):
+        """Size of this square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def tmp_update(self, id=None, size=None, x=None, y=None):
+        """that assigns an argument to each attribute:"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """Returns dictionary of the class"""
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
