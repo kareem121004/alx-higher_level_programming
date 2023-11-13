7d4f75b2df4872ac0118ca0557fa945225a3a2e3
kareem121004 <karimadel11112004@gmail.com>
2023-11-13 08:33:57 -0800

    commit again

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,6 +69,3 @@ class Rectangle(Base):
             raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0:
             raise ValueError("{} must be > 0".format(name))
-
-    def area(self):
-        return self.width * self.height
