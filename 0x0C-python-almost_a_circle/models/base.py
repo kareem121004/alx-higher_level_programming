c877be2c8c30572bfeab525ee5b91aa4e6a14777
kareem121004 <karimadel11112004@gmail.com>
2023-11-13 00:11:53 -0800

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 """This module implements `base` class"""
+
+import json
 
 
 class Base:
@@ -14,3 +16,41 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """list to json"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """transform a JSON string"""
+        if json_string is None or not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create a new object from dictionary"""
+        if cls.__name__ == "Rectangle":
+            new = cls(10, 10)
+        elif cls.__name__ == "Square":
+            new = cls(10, 10)
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to json file"""
+
+        filename = cls.__name__ + ".json"
+        word = []
+
+        if list_objs is not None:
+            for lst in list_objs:
+                word.append(lst.to_dictionary())
+            with open(filename, mode="w", encoding="utf-8") as f:
+                return f.write(Base.to_json_string(word))
