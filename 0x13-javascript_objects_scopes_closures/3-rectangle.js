4afaac1c668f072d652aad20090e03613c795d51
kareem121004 <karimadel11112004@gmail.com>
2024-02-13 08:24:59 -0800

    commit

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,8 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-	    this.width = 0;
-	    this.height = 0;
+	    return;
     } else {
       this.width = w;
       this.height = h;
